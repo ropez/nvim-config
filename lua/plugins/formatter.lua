442f8ca0a430c8620b3089fc109d637db2944d13
Robin Pedersen <robin.pedersen@visma.com>
2024-09-04 10:31:33 +0200

    Formatters

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -4,12 +4,14 @@ return {
   opts = {
     filetype = {
       rust = 'rustfmt --edition 2021',
+      html = 'prettierd .html',
       typescript = 'prettierd .ts',
       javascipt = 'prettierd .js',
       vue = 'prettierd .vue',
       svelte = 'prettier --write --stdin-filepath .svelte',
       graphql = 'prettierd .gql',
       cs = 'dotnet csharpier',
+      dart = 'dart format --line-length=120 -o show'
     }
   },
 
