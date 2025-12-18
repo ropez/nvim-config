274c0e7116c77b9079a8350f4aaf3ebd908aa10d
Robin Pedersen <robinpeder@gmail.com>
2025-12-18 13:49:59 +0100

    Add other dotfiles for saving

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,6 +4,23 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
+    vim.api.nvim_command([[
+        augroup ChangeBackgroudColour
+            autocmd colorscheme * :hi normal guibg=#0A0721
+
+            " Inactive window
+            autocmd colorscheme * :hi NormalNC guibg=#0A0721
+
+            " Status line
+            autocmd colorscheme * :hi StatusLine guibg=#131033
+            autocmd colorscheme * :hi StatusLineNC guibg=#131033
+
+            " Lua line
+            autocmd colorscheme * :hi LineNr guibg=#131033
+
+        augroup END
+    ]])
+    -- vim.o.termguicolors = true
     vim.cmd [[silent! colorscheme catppuccin-mocha]]
   end,
 }
